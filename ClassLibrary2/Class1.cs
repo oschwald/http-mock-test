9b9458352d0881ffe3c21785985be14c4118f4e1
Gregory Oschwald <goschwald@maxmind.com>
2015-09-06 11:24:47 -0700

    Test case for Kayak issue

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,30 @@
+﻿using HttpMock;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        [Test]
+        public void SUT_should_return_stubbed_response()
+        {
+            var stubHttp = HttpMockRepository.At("http://localhost:9191");
+
+            const string expected = "<xml><>response>Hello World</response></xml>";
+            stubHttp.Stub(x => x.Get("/endpoint"))
+                    .Return(expected)
+                    .OK();
+
+            string result = new WebClient().DownloadString("http://localhost:9191/endpoint");
+
+            Console.WriteLine("RESPONSE: {0}", result);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
